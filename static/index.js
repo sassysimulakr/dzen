3c460f0d4d1ec9c46b6b21266dfc3fa183f8c5b7
Ihewaz <ihewaz@mail.ru>
2017-03-20 14:10:20 +0300

    улучшил стиль
    картинка и текст отлично центрируются и смотрятся sassy :fire:

--- static/index.js
+++ static/index.js
@@ -1,9 +1,13 @@
 $(document).ready(
-  function() {
-    $('#text').click(function() {
-        $.get("", function(data) {
-            $("#text").html(data);
+    function() {
+        $('#text').click(function() {
+            $('#preImg').remove(function() {
+                $.get("", function(data) {
+                    setTimeout(function() {
+                        $("#text").html(data)
+                    }, 2000);
+                })
+            })
         })
-    })
-  }
+    }
 )
