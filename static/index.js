81e2ed6e74ff161f4fcda76716a899e11191192d
Ihewaz <ihewaz@mail.ru>
2017-03-20 14:12:51 +0300

    убрал баг из index.js
    не обновил зависимотси с img

--- static/index.js
+++ static/index.js
@@ -1,12 +1,10 @@
 $(document).ready(
     function() {
         $('#text').click(function() {
-            $('#preImg').remove(function() {
-                $.get("", function(data) {
-                    setTimeout(function() {
-                        $("#text").html(data)
-                    }, 2000);
-                })
+            $.get("", function(data) {
+                setTimeout(function() {
+                    $("#text").html(data)
+                }, 2000);
             })
         })
     }
