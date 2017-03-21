ec3a87824ef54df1176fe9a739f92332487b6bfa
Ihewaz <ihewaz@mail.ru>
2017-03-21 18:36:41 +0300

    сокеты 1
    app.listen vs http.listen - важно!!
    инициализировать сокеты нужно через http.listen или переменной
    var dasdasd = app.listen(1488)
    
    но нельзя просто сказать app.listen() - так сокеты работать не будут

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 fs = require('fs');
 var pub = __dirname;
 
@@ -162,7 +164,13 @@ app.get('/toSend', function(req, res) {
     });
 });
 
-app.listen(3001, function() {
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+});
+
+http.listen(3001, function() {
     console.log('App listening on port 3001!');
 });
 
--- static/index.js
+++ static/index.js
@@ -7,3 +7,7 @@ $(document).ready(
         })
     }
 )
+
+
+var socket = io();
+console.log(socket);
